f34017a54f96472a6ae6685579a8e16622629b26
Abdelhdi9 <mostavqzr@gmail.com>
2025-10-01 16:39:08 +0100

    تحويل الصفحة للاحترافية وإضافة الخلفية المتحركة

--- app.py
+++ app.py
@@ -1,41 +1,16 @@
-from flask import Flask, request, jsonify, render_template
-
-app = Flask(__name__)
-
-# الصفحة الرئيسية
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-# API للمستخدمين (اللي عملناه سابقاً)
-users = []
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    return jsonify(users)
-
-@app.route('/users', methods=['POST'])
-def add_user():
-    data = request.get_json()
-    user = {"id": len(users) + 1, "name": data["name"], "email": data["email"]}
-    users.append(user)
-    return jsonify(user), 201
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
 from flask import Flask, request, render_template, jsonify
 import psycopg2
 from datetime import datetime
 
 app = Flask(__name__)
 
-# الاتصال بقاعدة البيانات على Render
+# الاتصال بقاعدة البيانات
 conn = psycopg2.connect(
     "postgresql://myuser:GhqaE0hRm8rM2fL026X8QEu0wQi599F2@dpg-d3eigqogjchc738p4mqg-a.frankfurt-postgres.render.com/mydb_zpih"
 )
 cur = conn.cursor()
 
+# الصفحة الرئيسية
 @app.route('/')
 def home():
     # تسجيل الزيارة
@@ -50,16 +25,35 @@ def home():
     
     return render_template('index.html')
 
-@app.route('/visits')
+# API للمستخدمين
+users = []
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    user = {"id": len(users) + 1, "name": data["name"], "email": data["email"]}
+    users.append(user)
+    return jsonify(user), 201
+
+# API للزيارات
+@app.route('/visits', methods=['GET'])
 def get_visits():
     cur.execute("SELECT id, ip_address, user_agent, visit_time FROM visits ORDER BY visit_time DESC;")
     rows = cur.fetchall()
     visits_list = []
-for visit in visits_from_db:
-    visits_list.append({
-        "id": visit[0],
-        "ip_address": visit[1],
-        "user_agent": visit[2],
-        "visit_time": str(visit[3])
-    })
+    for visit in rows:
+        visits_list.append({
+            "id": visit[0],
+            "ip_address": visit[1],
+            "user_agent": visit[2],
+            "visit_time": str(visit[3])
+        })
+    return jsonify(visits_list)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
